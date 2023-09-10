22f52aa1c60a70d299071be0f15c5d10c2bfd123
Rishabh <rishujain0721@gmail.com>
2023-09-10 19:05:51 +0530

    Corrected cors

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ import routes from "./routes/routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://dorm-deals-backend-rpnw.vercel.app",
+  })
+);
+app.options("*", cors());
+
 app.use(express.json());
 
-app.use('/', routes);
+app.use("/", routes);
 
 mongoose
-  .connect("mongodb+srv://rishujain0721:1234560@cluster0.diqnynf.mongodb.net/ecommerce")
+  .connect(
+    "mongodb+srv://rishujain0721:1234560@cluster0.diqnynf.mongodb.net/ecommerce"
+  )
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => {
