06c949f0a3f30c43c78e7278d57e3abb2ae9dc53
Rishabh <rishujain0721@gmail.com>
2023-10-05 11:51:00 +0530

    Changed cors methods

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import routes from "./routes/routes.js";
-import cron from 'node-cron';
+import cron from "node-cron";
 import User from "./models/UserModel.js";
 import "dotenv/config.js";
 
@@ -11,16 +11,17 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: "https://dorm-deals-frontend.vercel.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
-const PASSWORD=process.env.PASS;
+const PASSWORD = process.env.PASS;
 
 app.use(express.json());
 
 app.use("/", routes);
 
-// Schedule the cleanup task to run every minute 
-cron.schedule('* * * * *', async () => {
+// Schedule the cleanup task to run every minute
+cron.schedule("* * * * *", async () => {
   const fiveMinutesAgo = new Date();
   fiveMinutesAgo.setMinutes(fiveMinutesAgo.getMinutes() - 5);
 
@@ -30,13 +31,14 @@ cron.schedule('* * * * *', async () => {
     createdAt: { $lt: fiveMinutesAgo },
   });
 
-  console.log("Users going to be deleted : ",unverifiedUsers)
+  console.log("Users going to be deleted : ", unverifiedUsers);
   // Delete unverified user records
-  await User.deleteMany({ _id: { $in: unverifiedUsers.map((user) => user._id) } });
-  
-  console.log('Cleanup task completed.');
-});
+  await User.deleteMany({
+    _id: { $in: unverifiedUsers.map((user) => user._id) },
+  });
 
+  console.log("Cleanup task completed.");
+});
 
 mongoose
   .connect(
