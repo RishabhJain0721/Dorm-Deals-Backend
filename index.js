04768fd87a49d5455b0a5c4c3f34357f68b7968f
Rishabh <rishujain0721@gmail.com>
2023-12-26 18:33:55 +0530

    Final commit

--- index.js
+++ index.js
@@ -10,18 +10,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_CONNECTION_STRING;
 
-// const corsMiddleware = (req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://dorm-deals-frontend.vercel.app')
-//       .header('Access-Control-Allow-Headers', 'Authorization,Accept,Origin,DNT,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range')
-//       .header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,DELETE,PATCH');
-
-//   if (req.method === 'OPTIONS') {
-//       res.sendStatus(200);
-//   } else {
-//       next();
-//   }
-// }
-
 app.use(cors());
 
 app.use(express.json());
